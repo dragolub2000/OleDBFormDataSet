d21eb70ef8b39936d9e778757a2ce17cc428b398
Dragan Golubovic <dragolub2000@yahoo.com>
2022-10-02 17:17:01 +0200

    Verzija 1.0.0

---
+++ OleDBFormDataSet/OleDBFormDataSet/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace OleDBFormDataSet
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnPrikazi = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPrezime = new System.Windows.Forms.TextBox();
+            this.txtIme = new System.Windows.Forms.TextBox();
+            this.lvRadnici = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 343);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 16);
+            this.label3.TabIndex = 26;
+            this.label3.Text = "ID";
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(108, 337);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(95, 22);
+            this.txtID.TabIndex = 25;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(425, 100);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(242, 23);
+            this.button2.TabIndex = 24;
+            this.button2.Text = "Измени";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(425, 56);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(242, 23);
+            this.button1.TabIndex = 23;
+            this.button1.Text = "Додај";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnPrikazi
+            // 
+            this.btnPrikazi.Location = new System.Drawing.Point(425, 12);
+            this.btnPrikazi.Name = "btnPrikazi";
+            this.btnPrikazi.Size = new System.Drawing.Size(242, 23);
+            this.btnPrikazi.TabIndex = 22;
+            this.btnPrikazi.Text = "Прикажи";
+            this.btnPrikazi.UseVisualStyleBackColor = true;
+            this.btnPrikazi.Click += new System.EventHandler(this.btnPrikazi_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 434);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 16);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "Преѕиме";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 393);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 16);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "Име";
+            // 
+            // txtPrezime
+            // 
+            this.txtPrezime.Location = new System.Drawing.Point(108, 431);
+            this.txtPrezime.Name = "txtPrezime";
+            this.txtPrezime.Size = new System.Drawing.Size(559, 22);
+            this.txtPrezime.TabIndex = 19;
+            // 
+            // txtIme
+            // 
+            this.txtIme.Location = new System.Drawing.Point(108, 387);
+            this.txtIme.Name = "txtIme";
+            this.txtIme.Size = new System.Drawing.Size(559, 22);
+            this.txtIme.TabIndex = 18;
+            // 
+            // lvRadnici
+            // 
+            this.lvRadnici.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.lvRadnici.HideSelection = false;
+            this.lvRadnici.Location = new System.Drawing.Point(12, 12);
+            this.lvRadnici.Name = "lvRadnici";
+            this.lvRadnici.Size = new System.Drawing.Size(393, 308);
+            this.lvRadnici.TabIndex = 17;
+            this.lvRadnici.UseCompatibleStateImageBehavior = false;
+            this.lvRadnici.View = System.Windows.Forms.View.Details;
+            this.lvRadnici.Click += new System.EventHandler(this.lvRadnici_Click);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "ID";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Ime";
+            this.columnHeader2.Width = 120;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Prezime";
+            this.columnHeader3.Width = 160;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(677, 484);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnPrikazi);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPrezime);
+            this.Controls.Add(this.txtIme);
+            this.Controls.Add(this.lvRadnici);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnPrikazi;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPrezime;
+        private System.Windows.Forms.TextBox txtIme;
+        private System.Windows.Forms.ListView lvRadnici;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+    }
+}
+
---
+++ OleDBFormDataSet/OleDBFormDataSet/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OleDBFormDataSet
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void ShowForm()
+        {
+            try
+            {
+                OleDbConnection conn = new OleDbConnection();
+                conn.ConnectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=c:\\testdatabase.xls;Extended Properties=\"Excel 8.0\"";
+                String strSQL = "Select * from Radnik2";
+                // Kreiranje  OleDbDataAdapter objekta  
+                OleDbDataAdapter adapter = new OleDbDataAdapter();
+                adapter.SelectCommand = new OleDbCommand(strSQL, conn);
+                // Kreiranje Data Set objekta!
+                DataSet ds = new DataSet();
+                adapter.Fill(ds, "Radnici");
+                DataTable table = ds.Tables[0];
+                for (int i = 0; i < table.Rows.Count;i++)
+                {
+                    this.lvRadnici.Items.Add(table.Rows[i][0].ToString());
+                    this.lvRadnici.Items[i].SubItems.Add(table.Rows[i][1].ToString());
+                    this.lvRadnici.Items[i].SubItems.Add(table.Rows[i][2].ToString());
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Postoji izuzetak" + ex.Message);
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.ShowForm();
+        }
+
+        private void btnPrikazi_Click(object sender, EventArgs e)
+        {
+            this.ShowForm();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OleDbConnection conn = new OleDbConnection();
+                String selectQuery = "Select * from Radnik2";
+                string insertQuery = "INSERT INTO Radnik2 (id,Ime,Prezime)  VALUES (@id,@Ime,@Prezime)";
+                conn.ConnectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=c:\\testdatabase.xls;Extended Properties=\"Excel 8.0\"";
+                OleDbDataAdapter adapter = new OleDbDataAdapter(selectQuery, conn);
+                DataSet ds = new DataSet();
+
+                adapter.Fill(ds, "Radnici");
+
+                int id = Int32.Parse(txtID.Text);
+                string Ime = txtIme.Text;
+                string Prezime = txtPrezime.Text;
+                // dodavanje nove vrste u tabelu
+                DataTable table = ds.Tables[0];
+                DataRow newRow = ds.Tables[0].NewRow();
+                newRow[0] = id;
+                newRow[1] = Ime;
+                newRow[2] = Prezime;
+                table.Rows.Add(newRow);
+
+                adapter.InsertCommand = new OleDbCommand(insertQuery,conn);
+                adapter.InsertCommand.Parameters.AddWithValue("@id", id);
+                adapter.InsertCommand.Parameters.AddWithValue("@Ime", Ime);
+                adapter.InsertCommand.Parameters.AddWithValue("@Prezime", Prezime);
+
+                // osvezavanje/update  adaptera sa novim DataSource-om ds
+                adapter.Update(ds, "Radnici");
+                // brisanje svih stavki iz this.lvRadnici.Items
+                // a zatim novo prikazivanje listview-a lvRadnici sa novim podacima
+                this.lvRadnici.Items.Clear();
+                for (int i = 0; i < table.Rows.Count; i++)
+                {
+                    this.lvRadnici.Items.Add(table.Rows[i][0].ToString());
+                    this.lvRadnici.Items[i].SubItems.Add(table.Rows[i][1].ToString());
+                    this.lvRadnici.Items[i].SubItems.Add(table.Rows[i][2].ToString());
+                }
+                MessageBox.Show("Podatak dodat!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Postoji izuzetak" + ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OleDbConnection conn = new OleDbConnection();
+                String selectQuery = "Select * from Radnik2";
+                string updateQuery = "UPDATE Radnik2 SET ID=@id,Ime=@Ime,Prezime=@Prezime WHERE ID=@id";
+                conn.ConnectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=c:\\testdatabase.xls;Extended Properties=\"Excel 8.0\"";
+                OleDbDataAdapter adapter = new OleDbDataAdapter(selectQuery, conn);
+                DataSet ds = new DataSet();
+
+                adapter.Fill(ds, "Radnici");
+
+                int id = Int32.Parse(txtID.Text);
+                string Ime = txtIme.Text;
+                string Prezime = txtPrezime.Text;
+                // update vrste u tabeli
+                DataTable table = ds.Tables[0];
+                DataRow updatedRow = ds.Tables[0].Rows[this.lvRadnici.SelectedIndices[0]];
+                updatedRow[0] = id;
+                updatedRow[1] = Ime;
+                updatedRow[2] = Prezime;
+
+                adapter.UpdateCommand = new OleDbCommand(updateQuery, conn);
+                adapter.UpdateCommand.Parameters.AddWithValue("@id", id);
+                adapter.UpdateCommand.Parameters.AddWithValue("@Ime", Ime);
+                adapter.UpdateCommand.Parameters.AddWithValue("@Prezime", Prezime);
+
+                // osvezavanje/update  adaptera sa novim DataSource-om ds
+                adapter.Update(ds, "Radnici");
+                // brisanje svih stavki iz this.lvRadnici.Items
+                // a zatim novo prikazivanje listview-a lvRadnici sa novim podacima
+                this.lvRadnici.Items.Clear();
+                for (int i = 0; i < table.Rows.Count; i++)
+                {
+                    this.lvRadnici.Items.Add(table.Rows[i][0].ToString());
+                    this.lvRadnici.Items[i].SubItems.Add(table.Rows[i][1].ToString());
+                    this.lvRadnici.Items[i].SubItems.Add(table.Rows[i][2].ToString());
+                }
+                MessageBox.Show("Podatak izmenjen!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Postoji izuzetak" + ex.Message);
+            }
+        }
+
+        private void lvRadnici_Click(object sender, EventArgs e)
+        {
+            var item = this.lvRadnici.SelectedItems[0];
+            string Ime = item.SubItems[1].Text;
+            string Prezime = item.SubItems[2].Text;
+
+            this.txtID.Text = item.SubItems[0].Text;
+            this.txtIme.Text = Ime;
+            this.txtPrezime.Text = Prezime;
+        }
+    }
+}
